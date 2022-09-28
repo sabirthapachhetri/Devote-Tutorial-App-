58d42cf60dfa09a1534a0336a478617513ce4e2e
Sabir <sabirthapa19@gmail.com>
2022-09-28 19:50:29 +0545

    intro to core data

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Sabir's MacBook on 9/28/22.
+//
+
+import SwiftUI
+
+// FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+
+// UI
+
+
+// UX
