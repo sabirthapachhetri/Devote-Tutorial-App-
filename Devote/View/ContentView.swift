81147c73f7c8bfb08f862441edddf58a799af077
Sabir <sabirthapa19@gmail.com>
2022-10-20 08:02:05 +0545

    added blur effect

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -113,11 +113,16 @@ struct ContentView: View {
                             .padding(.vertical, 0)
                             .frame(maxWidth: 640)
                 }
+                    .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeOut(duration: 0.5))
                     
                     // NEW TASK ITEM
                     if showNewTaskItem {
                         
-                        BlankView()
+                        BlankView(
+                            backgroundColor: isDarkMode ? Color.black : Color.gray,
+                            backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                             .onTapGesture {
                                 withAnimation(){
                                     showNewTaskItem = false
@@ -133,6 +138,7 @@ struct ContentView: View {
                 .navigationBarHidden(true)
                 .background(
                     BackgroundImageView()
+                        .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
                 )
                 .background(
                     backgroundGradient.ignoresSafeArea(.all)
